fe069ee3272239c5c1d81fdc4dc0b9691330dae1
Student_11 <jhonymaurad@gmail.com>
2019-01-05 10:19:20 -0500

    added api/ scope for data endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import axios from 'axios';
+
+async function getMatches() {
+  const resp = await axios.get('/api/matches');
+  console.log(resp.data);
+}
+
+getMatches();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        hello world
       </div>
     );
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  post 'coach_token' => 'coach_token#create'
-  resources :coaches
-  resources :matches do
-    collection do
-      get 'mine'
+  scope 'api/' do
+    post 'coach_token' => 'coach_token#create'
+    resources :coaches
+    resources :matches do
+      collection do
+        get 'mine'
+      end
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
